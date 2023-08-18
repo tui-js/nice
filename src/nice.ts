2c2e5fa4a0ba308e02cb6a456d25649eeab4e8e1
Im-Beast <franik.mateusz@gmail.com>
2023-08-18 22:10:47 +0200

    fix: ellipsis overflowing drawn object

--- src/nice.ts
+++ src/nice.ts
@@ -136,18 +136,20 @@ export class Nice {
             const ellipsisString = text.ellipsisString ?? "…";
             const ellipsisWidth = textWidth(ellipsisString);
 
+            const expectedWidth = width - ellipsisWidth;
+
             if (lineWidth === width) {
-              textLines[i] = cropToWidth(textLine, width - ellipsisWidth) + ellipsisString;
+              textLines[i] = cropToWidth(textLine, expectedWidth) + ellipsisString;
             } else if (lineWidth > 0) {
-              textLines[i] += ellipsisString;
+              textLines[i] = cropToWidth(textLine, expectedWidth) + ellipsisString;
             } else {
               const lastLine = textLines[i - 1];
               const lastLineWidth = textWidth(lastLine);
 
               if (lastLineWidth === width) {
-                textLines[i - 1] = cropToWidth(lastLine, width - ellipsisWidth) + ellipsisString;
+                textLines[i - 1] = cropToWidth(lastLine, expectedWidth) + ellipsisString;
               } else {
-                textLines[i - 1] = cropToWidth(lastLine, width - ellipsisWidth) + ellipsisString;
+                textLines[i - 1] = cropToWidth(lastLine, expectedWidth) + ellipsisString;
               }
             }
           }
