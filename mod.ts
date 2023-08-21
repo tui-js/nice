25eb9707dbae5589e9aa068e178503c8291afe6a
Im-Beast <franik.mateusz@gmail.com>
2023-08-21 21:15:02 +0200

    fix: mod.ts reexport directory

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 // Copyright 2023 Im-Beast. All rights reserved. MIT license.
 export * from "./src/border.ts";
 export * from "./src/nice.ts";
-export * from "./src/utils.ts";
+export * from "./src/utils/mod.ts";
