1e854f58481608e67d6718621d81ac37202be817
Im-Beast <franik.mateusz@gmail.com>
2024-07-03 16:45:35 +0200

    chore: `deno fmt`

--- tests/style/render_relative.ts
+++ tests/style/render_relative.ts
@@ -3,30 +3,30 @@ import { TestCase } from "../nice-test-runner.ts";
 import { Nice } from "../../mod.ts";
 
 const style = new Nice({
-	border: {
-		all: true,
-		type: "rounded",
-		style: crayon.white.bold,
-	},
-	padding: { x: 3, y: 1 },
-	style: crayon.bgBlue,
+  border: {
+    all: true,
+    type: "rounded",
+    style: crayon.white.bold,
+  },
+  padding: { x: 3, y: 1 },
+  style: crayon.bgBlue,
 });
 
 function render() {
-	console.log(Nice.render(style.draw("Hello there")));
-	console.log(Nice.renderRelative(style.draw("Hello there")));
-	console.log("\x1b[10C", Nice.renderRelative(style.draw("Hello there")));
-	console.log("\x1b[10A\x1b[30C", Nice.renderRelative(style.draw("Hello there")), "\x1b[10B");
+  console.log(Nice.render(style.draw("Hello there")));
+  console.log(Nice.renderRelative(style.draw("Hello there")));
+  console.log("\x1b[10C", Nice.renderRelative(style.draw("Hello there")));
+  console.log("\x1b[10A\x1b[30C", Nice.renderRelative(style.draw("Hello there")), "\x1b[10B");
 }
 
 export const testCase = new TestCase(
-	"Nice.renderRelative",
-	crayon`\
+  "Nice.renderRelative",
+  crayon`\
 {bold This test showcases rendering via Nice.renderRelative.}
 Rendered blocks should not have any glitches.`,
-	render,
+  render,
 );
 
 if (import.meta.main) {
-	testCase.run();
+  testCase.run();
 }
