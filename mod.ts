e9e1972470f1ff67105a4b2d5d2c02dfdb096b12
Im-Beast <franik.mateusz@gmail.com>
2024-07-03 16:26:17 +0200

    feat: export individual entries, export everything in main entry

--- mod.ts
+++ mod.ts
@@ -1,4 +1,9 @@
 // Copyright 2023 Im-Beast. All rights reserved. MIT license.
-export * from "./src/border/mod.ts";
 export * from "./src/nice.ts";
-export * from "./src/utils/mod.ts";
+export * from "./src/metadata.ts";
+
+export * from "./src/layout/mod.ts";
+
+export * from "./src/border/mod.ts";
+export * from "./src/margin/mod.ts";
+export * from "./src/text/mod.ts";
