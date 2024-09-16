3c8bc183993b9b5e20e512b45538d11da6b10f6c
Im-Beast <franik.mateusz@gmail.com>
2024-09-16 17:11:50 +0200

    chore: `deno fmt`

--- src/layout_block.ts
+++ src/layout_block.ts
@@ -2,66 +2,66 @@ import { signal } from "@tui/signals";
 import { Block } from "./block.ts";
 
 export class LayoutBlock extends Block {
-    name = "Layout";
+  name = "Layout";
 
-    usedWidth = 0;
-    usedHeight = 0;
+  usedWidth = 0;
+  usedHeight = 0;
 
-    compute(parent: Block): void {
-        super.compute(parent);
-        this.usedWidth = 0;
-        this.usedHeight = 0;
+  compute(parent: Block): void {
+    super.compute(parent);
+    this.usedWidth = 0;
+    this.usedHeight = 0;
+  }
+
+  draw() {
+    let { parent } = this;
+    if (!parent) {
+      const width = signal(0);
+      const height = signal(0);
+
+      const resize = () => {
+        const { columns, rows } = Deno.consoleSize();
+        width.set(columns);
+        height.set(rows);
+      };
+
+      resize();
+
+      // Because SIGWINCH signal is not supported on windows
+      // We just have to check whether size has changed in a loop
+      if (Deno.build.os === "windows") {
+        Deno.unrefTimer(setInterval(resize, 32));
+      } else {
+        Deno.addSignalListener("SIGWINCH", resize);
+      }
+
+      parent = new Block({
+        id: "terminal",
+        width,
+        height,
+        children: [this],
+      });
     }
 
-    draw() {
-        let { parent } = this;
-        if (!parent) {
-            const width = signal(0);
-            const height = signal(0);
+    this.compute(parent);
 
-            const resize = () => {
-                const { columns, rows } = Deno.consoleSize();
-                width.set(columns);
-                height.set(rows);
-            };
-
-            resize();
-
-            // Because SIGWINCH signal is not supported on windows
-            // We just have to check whether size has changed in a loop
-            if (Deno.build.os === "windows") {
-              Deno.unrefTimer(setInterval(resize, 32));
-            } else {
-              Deno.addSignalListener("SIGWINCH", resize);
-            }
-
-            parent = new Block({
-                id: "terminal",
-                width,
-                height,
-                children: [this],
-            });
-        }
-
-        this.compute(parent);
-
-        if (this.children) {
-            this.startLayout(parent);
-            for (const child of this.children) {
-                this.layout(child);
-            }
-            this.finishLayout(parent);
-        }
-
-        this.changed = false;
+    if (this.children) {
+      this.startLayout(parent);
+      for (const child of this.children) {
+        this.layout(child);
+      }
+      this.finishLayout(parent);
     }
 
-    startLayout(parent: Block): void;
-    startLayout(): void {}
+    this.changed = false;
+  }
 
-    finishLayout(parent: Block): void;
-    finishLayout(): void {}
+  startLayout(parent: Block): void;
+  startLayout(): void {}
 
-    layout(child: Block): void;
-    layout(): void {}
+  finishLayout(parent: Block): void;
+  finishLayout(): void {}
+
+  layout(child: Block): void;
+  layout(): void {}
 }
